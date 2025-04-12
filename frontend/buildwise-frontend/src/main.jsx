cb8d634d586b894b63e368958e792433f803b44a
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-04-12 11:07:01 +0530

    .

--- frontend/buildwise-frontend/src/main.jsx
+++ frontend/buildwise-frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Authcontext"; // Assuming this is correct path
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
